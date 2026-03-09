a0cdc2042bbb87aa024560687523eb37847d8d35
Abhilasha Jha <jhaabhilasha267@gmail.com>
2026-03-09 17:48:04 +0530

    modified:   pages/products/sdcvp-x.js #	new file:   public/sdcvp-x.pdf

--- pages/products/sdcvp-x.js
+++ pages/products/sdcvp-x.js
@@ -272,23 +272,23 @@ export default function SDCVPXPage() {
   ];
 
   // Define the brochure download function
-  // const downloadBrochure = () => {
-  //   const link = document.createElement("a");
-  //   link.href = "/SdCvP‑X™️VeripointTechnologiesBrochure.pdf"; // Ensure this file is in your public folder
-  //   link.download = "SdCvP‑X™️VeripointTechnologiesBrochure.pdf";
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   document.body.removeChild(link);
-  // };
- const downloadBrochure = () => {
-  const link = document.createElement("a");
-  link.href =
-    "https://github.com/abhilasha-2302/vt-web/releases/download/v0.1/tool.rpm";
-  link.download = "abc.rpm";
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
+  const downloadBrochure = () => {
+    const link = document.createElement("a");
+    link.href = "/sdcvp-x.pdf"; // Ensure this file is in your public folder
+    link.download = "sdcvp-x.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+//  const downloadBrochure = () => {
+//   const link = document.createElement("a");
+//   link.href =
+//     "https://github.com/abhilasha-2302/vt-web/releases/download/v0.1/tool.rpm";
+//   link.download = "abc.rpm";
+//   document.body.appendChild(link);
+//   link.click();
+//   document.body.removeChild(link);
+// };
   return (
     <Layout>
       <Head>
