a2d9279062b5245646314a7c16522875c8c73782
Abhilasha Jha <jhaabhilasha267@gmail.com>
2026-03-11 12:12:53 +0530

    modified:   pages/products/sdcvp-x.js

--- pages/products/sdcvp-x.js
+++ pages/products/sdcvp-x.js
@@ -283,8 +283,8 @@ export default function SDCVPXPage() {
  const downloadBrochure = () => {
   const link = document.createElement("a");
   link.href =
-    "https://github.com/abhilasha-2302/vt-web/releases/download/v1.0/abc.pdf";
-  link.download = "abc.pdf";
+    "https://github.com/abhilasha-2302/vt-web/releases/download/v1.0/tool.rpm";
+  link.download = "tool.rpm";
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
